37f854e395df99826176fa9a1251ae5c0f0270d0
xongtao <xongttao@gmail.com>
2024-04-24 08:47:25 +0800

    整理

--- uniswapv2-contracts/contracts/interfaces/V1/IUniswapV1Factory.sol
+++
@@ -1,6 +0,0 @@
-// SPDX-License-Identifier: GPL-3.0-only
-pragma solidity =0.8.13;
-
-interface IUniswapV1Factory {
-    function getExchange(address) external view returns (address);
-}
--- zuniswapv2_0.8.1/Makefile
+++
@@ -1,4 +0,0 @@
-test:
-	forge test -vvv
-codehash:
-	cat out/ZuniswapV2Pair.sol/ZuniswapV2Pair.json | jq -r .bytecode.object | xargs cast keccak
